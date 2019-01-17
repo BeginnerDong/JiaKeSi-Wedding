28f2281b6e478a087d82fb8c3d194eb9f23b37d9
Mr zh <877854886@qq.com>
2019-01-17 11:20:49 +0800

    test

--- pages/index/index.js
+++ pages/index/index.js
@@ -17,15 +17,70 @@ Page({
     nextMargin: 0,
     swiperIndex:0,
     swiperBig:0,
+    submitData:{
+      city:'',
+      district:''
+    },
+    sliderData:[],
+    isFirstLoadAllStandard:['getLocation','getSliderData']
   },
   
  
   onLoad() {
     const self = this;
-    self.data.paginate = getApp().globalData.paginate;
-
-
+    api.commonInit(self);
+    self.getLocation();
+    self.getSliderData()
   },
+
+  getSliderData(){
+    const self = this;
+    const postData = {};
+    postData.searchItem = {
+      thirdapp_id:getApp().globalData.thirdapp_id
+    };
+    postData.getBefore = {
+      caseData:{
+        tableName:'Label',
+        searchItem:{
+          title:['=',['首页轮播']],
+        },
+        middleKey:'parentid',
+        key:'id',
+        condition:'in',
+      },
+    };
+    postData.order = {
+      listorder:'desc'
+    };
+    const callback = (res)=>{ 
+      console.log(1000,res);
+      if(res.info.data.length>0){
+       self.data.sliderData.push.apply(self.data.sliderData,res.info.data)
+      }
+      self.setData({
+        web_sliderData:self.data.sliderData,
+      });
+      api.checkLoadAll(self.data.isFirstLoadAllStandard,'getSliderData',self);
+    };
+    api.labelGet(postData,callback);
+  },
+
+  getLocation() {
+    const self = this;
+    const callback = (res)=>{
+      if(res){
+        self.data.submitData.city = res.address_component.city,
+        self.data.submitData.district = res.address_component.district
+      };
+      self.setData({
+        web_submitData:self.data.submitData
+      })
+    };
+    api.getLocation('reverseGeocoder',callback);
+    api.checkLoadAll(self.data.isFirstLoadAllStandard,'getLocation',self)
+  },
+
   /**************轮播*****************/
   swiperChange(e) {
     const that = this;
